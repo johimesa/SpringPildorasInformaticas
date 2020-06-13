fbfb9ede322a2e9994be36cf6664593c3f3a5339
Hilario Medina <jmedinas@everis.com>
2020-06-12 19:06:07 -0500

    Se sube la primera clase

---
+++ ProjectSpringOne/src/es/pildoras/ioc/JefeEmpleado.java
@@ -0,0 +1,8 @@
+package es.pildoras.ioc;
+
+public class JefeEmpleado {
+
+	public String getTareas() {
+		return "Gestiona las cuestiones relativas a mis empleados de Sección.";
+	}
+}
---
+++ ProjectSpringOne/src/es/pildoras/ioc/UsoEmpleado.java
@@ -0,0 +1,14 @@
+package es.pildoras.ioc;
+
+public class UsoEmpleado {
+
+	public static void main(String[] args) {
+		// Creación del obj. Empleado
+		JefeEmpleado jefeEm = new JefeEmpleado();
+		
+		// Uso de los obj. creados
+		System.out.println(jefeEm.getTareas());
+
+	}
+
+}
