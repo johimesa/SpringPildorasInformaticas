6b65eda40817a4eb2359339694a593657110bb14
Hilario Medina <jmedinas@everis.com>
2020-06-29 20:15:21 -0500

    Java Annotations - @Configuration: Cómo configurar Spring con código Java prescindiendo de XML

---
+++ SpringAnnotations/src/pe/com/annotations/EmpleadosConfig.java
@@ -0,0 +1,12 @@
+package pe.com.annotations;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("pe.com.annotations")
+public class EmpleadosConfig {
+
+	
+	
+}
--- SpringAnnotations/src/pe/com/annotations/UsoAnnotations2.java
+++ SpringAnnotations/src/pe/com/annotations/UsoAnnotations2.java
@@ -1,13 +1,17 @@
 package pe.com.annotations;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 
 public class UsoAnnotations2 {
 
 	public static void main(String[] args) {
 		
 		// leer el xml de configuracion
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		//ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		// leer la clase de config.
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(EmpleadosConfig.class);
 		
 		// pedir un bean al contenedor
 		Empleado tony = context.getBean("ComercialExperimentado", Empleado.class);
